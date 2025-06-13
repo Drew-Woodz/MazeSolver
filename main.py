c7a938accaa26c20bc3c7b83b64c9795610394f2
Andrew Lockwood <andrew.lockwood578@myci.csuci.edu>
2025-06-13 00:39:55 -0500

    Add Kruskal's and Hunt-and-Kill maze generators with real-time Pygame visualization

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # main.py
 
 from visualizer.pygame_renderer import PygameRenderer
-from maze_generators.dfs_backtracker import generate_maze
+# from maze_generators.dfs_backtracker import generate_maze
 # from maze_generators.prims_algorithm import generate_maze
 # from maze_generators.wilsons_algorithm import generate_maze
 # from maze_generators.recursive_division import generate_maze
+# from maze_generators.hunt_and_kill import generate_maze
+from maze_generators.kruskals_algorithm import generate_maze
 
 if __name__ == "__main__":
     renderer = PygameRenderer(25, 25)
--- maze_generators/__init__.py
+++ maze_generators/__init__.py
@@ -3,11 +3,15 @@ from maze_generators.dfs_backtracker import generate_maze as dfs_backtracker
 from maze_generators.prims_algorithm import generate_maze as prims
 from maze_generators.wilsons_algorithm import generate_maze as wilsons
 from maze_generators.recursive_division import generate_maze as recursive_division
+from maze_generators.hunt_and_kill import generate_maze as hunt_and_kill
+from maze_generators.kruskals_algorithm import generate_maze as kruskals_algorithm
 
 generators = {
     "DFS Backtracker": dfs_backtracker,
     "Prim's Algorithm": prims,
     "Wilson's Algorithm": wilsons,
     "Recursive Division": recursive_division,
+    "Hunt and Kill": hunt_and_kill,
+    "Kruskal's Algorithm": kruskals_algorithm,
     # etc.
 }
